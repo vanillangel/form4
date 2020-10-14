eee4be266b7f23c3a3968342158dc29778e68651
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:58:56 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -2,7 +2,7 @@
 <?php
 $si_un=$_POST['si_username'];
 $si_pasw=$_POST['si_password'];
-echo 'Пользователь: ',$si_un,'Пароль: ',$si_pasw;
+echo 'Пользователь: ',$si_un,'<br>Пароль: ',$si_pasw;
 
 if (empty($si_un)) {
 	echo 'Имя пользователя не введено';
