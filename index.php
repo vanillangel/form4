d06faef41989f474a946b8d3652b790d1532268b
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 16:26:20 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<style type="text/css">
+		body{background-color: lightgray;}
+		form{border:1px solid black; width:300px; border-radius: 5px;
+			background-color: white;}
+	
+	</style>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Form</title>
+	<title>Document</title>
 </head>
 <body>
-	<a href="views/sign_in.php">Вход</a>
-	<a href="views/sign_up.php">Регистрация</a>
+	<form>
+
+	<h4><a href="views/sign_in.php">Sign In</a></h4>
+	<h4><a href="views/sign_up.php">Sign Up</a></h4>
+</form>
 </body>
 </html>
