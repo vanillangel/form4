325a3e0ef203a0391758fbbb154d49bc1b8a24b1
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 16:09:34 +0400

    Update sign_up.php

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -7,14 +7,14 @@ echo 'Пользователь: ',$su_un,'<br>E-mail адрес: ',$su_eadd,'<br
 
 if (empty($su_un)) {
 	echo 'Имя пользователя не введено';
-	header('Refresh: 5; url=http://localhost/views/sign_in.php');
+	header('Refresh: 5; url=http://localhost/views/sign_up.php');
 }
 if (empty($su_eadd)) {
 	echo 'Пароль не введен';
-	header('Refresh: 5; url=http://localhost/views/sign_in.php');
+	header('Refresh: 5; url=http://localhost/views/sign_up.php');
 }
 if (empty($su_pasw)) {
 	echo 'Пароль не введен';
-	header('Refresh: 5; url=http://localhost/views/sign_in.php');
+	header('Refresh: 5; url=http://localhost/views/sign_up.php');
 }
 ?>
