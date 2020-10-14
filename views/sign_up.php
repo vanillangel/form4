b8398fb7674067ff1deb138166d173d89dc6d379
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:02:04 +0400

    Update sign_up.php

--- views/sign_up.php
+++ views/sign_up.php
@@ -1,13 +1,28 @@
 <? php
 
-echo "<h4>Вход</h4>
-	<form method='POST'>
-		<h4 align='center'>Sign In</h4>
-		<input type='text' placeholder='username' name=''><br>
-<input type='password' placeholder='password' name=''>
-
-	</form>"
-
-
-
-?>
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style type="text/css">
+		body{background-color: lightgray;}
+		form{border:1px solid black; width:300px; border-radius: 5px;
+			background-color: white;}
+		form input{margin:10px;}
+		form button {background-color: lightgray; display: block;
+    margin: 0 auto 15px auto;
+    text-align: center;border-radius: 5px;}
+	</style>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form method="POST" action="/controllers/sign_up.php">
+		<h4 align="center">Sign Up/h4>
+		<input type="text" placeholder="username" name="si_username"><br>
+		<input type="text" placeholder="email address" name="si_eaddress"><br>
+<input type="password" placeholder="password" name="si_password"><br>
+<button script="index.php">Enter</button>
+	</form>
+</body>
+</html>
