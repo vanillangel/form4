e3ae77d661e138161acc900cdc8b935335ec4977
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 16:18:22 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -5,7 +5,7 @@ $si_pasw=$_POST['si_password'];
 echo 'Пользователь: ',$si_un,'<br>Пароль: ',$si_pasw;
 
 if (empty($si_un) ||empty($si_pasw)) {
-	echo '<br>Одно или несколько полей формы пустые';
+	echo '<br>!!!ATTENTION!!!<br>!!!ATTENTION!!!<br>!!!ATTENTION!!!<br>Одно или несколько полей формы пустые';
 	header('Refresh: 5; url=http://localhost/views/sign_in.php');
 }
 
