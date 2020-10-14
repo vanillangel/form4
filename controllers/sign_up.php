ce2097a12486da97b4aeb14dd716b1be8e55df6e
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 16:12:40 +0400

    Update sign_up.php

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -5,16 +5,9 @@ $su_eadd=$_POST['su_eaddress'];
 $su_pasw=$_POST['su_password'];
 echo 'Пользователь: ',$su_un,'<br>E-mail адрес: ',$su_eadd,'<br>Пароль: ',$su_pasw;
 
-if (empty($su_un)) {
-	echo 'Имя пользователя не введено';
+if (empty($su_un) || empty($su_eadd) ||empty($su_pasw)) {
+	echo 'одно или несколько полей формы пустые';
 	header('Refresh: 5; url=http://localhost/views/sign_up.php');
 }
-if (empty($su_eadd)) {
-	echo 'Пароль не введен';
-	header('Refresh: 5; url=http://localhost/views/sign_up.php');
-}
-if (empty($su_pasw)) {
-	echo 'Пароль не введен';
-	header('Refresh: 5; url=http://localhost/views/sign_up.php');
-}
+
 ?>
