cb35c6d0b5e7c79cde1ab0323dc1295c91ebd7f4
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:56:15 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -1,12 +1,15 @@
+
 <?php
 $si_un=$_POST['si_username'];
 $si_pasw=$_POST['si_password'];
 echo 'Пользователь: ',$si_un,'Пароль: ',$si_pasw;
-$stroke="Пользователь: $si_un,Пароль: $si_pasw";
-echo $stroke;
 
-$file = '/home/dfg/code/users.txt';
-file_put_contents($file, $stroke, FILE_APPEND);
+if (empty($si_un)) {
+	echo 'Имя пользователя не введено';
+	<a href="#" OnClick="history.back();">Назад</a>
 
-
+if (empty($si_pasw)) {
+	echo 'Пароль не введен';
+	<a href="#" OnClick="history.back();">Назад</a>
+}
 ?>
