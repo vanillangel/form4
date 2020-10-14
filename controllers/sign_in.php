a06c91ead09afeaa5bc94482d27eb3927b6627ea
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:57:20 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -7,7 +7,7 @@ echo 'Пользователь: ',$si_un,'Пароль: ',$si_pasw;
 if (empty($si_un)) {
 	echo 'Имя пользователя не введено';
 	<a href="#" OnClick="history.back();">Назад</a>
-
+}
 if (empty($si_pasw)) {
 	echo 'Пароль не введен';
 	<a href="#" OnClick="history.back();">Назад</a>
