e30b0a90579364399ea890ccef2e0277304802ca
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 15:25:30 +0400

    Update sign_in.php

--- controllers/sign_in.php
+++ controllers/sign_in.php
@@ -1,7 +1,9 @@
 <? php
-$si_un=$_POST['si_username']
-$si_pasw=$_POST['si_password']
-$filename = '/users.txt';
-file_put_contents($filename, "Пользователь: "+$si_un+"Пароль: "+$si_pasw,FILE_APPEND);
-
+$si_un=$_POST['si_username'];
+$si_pasw=$_POST['si_password'];
+$stroke="Пользователь: "+$si_un+"Пароль: "+$si_psw;
+echo $stroke;
+$fd = fopen("/users.txt", 'w') or die("не удалось создать файл");
+fwrite($fd, $stroke);
+fclose($fd);
 ?>
