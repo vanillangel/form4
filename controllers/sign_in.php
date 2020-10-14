ce5526e302aed9629a33328d366fc5486d8378af
vanillangel <72797966+vanillangel@users.noreply.github.com>
2020-10-14 14:17:27 +0400

    Add files via upload

---
+++ controllers/sign_in.php
@@ -0,0 +1,13 @@
+<? php
+
+echo "<h4>Вход</h4>
+	<form method='POST'>
+		<h4 align='center'>Sign In</h4>
+		<input type='text' placeholder='username' name=''><br>
+<input type='password' placeholder='password' name=''>
+
+	</form>"
+
+
+
+?>
